b4ebdc1ed3ee4ffe75ba299cf8b59f09a9f874fe
V1A <V1A@D58-V1A-SWK>
2017-02-17 10:41:58 -0500

    Changes in main

--- HW_Prog1/main.cpp
+++ HW_Prog1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <sstream>
 
-
+using namespace std;
 int main(int argc, char** argv) {
+	if (argc != 5){
+		cout << "Incorrect input args list" << endl;
+	}
+	
+	istringstream ss1(argv[2]);
+	int numPoints;
+	if (!(ss1 >> numPoints))
+		cerr << "Invalid number for numPoints" << argv[2] << '\n';
+
+	istringstream ss2(argv[3]);
+	int numTrials;
+	if (!(ss2 >> numTrials))
+		cerr << "Invalid number for numTrials" << argv[3] << '\n';
+
+	istringstream ss3(argv[4]);
+	int dimension;
+	if (!(ss3 >> dimension))
+		cerr << "Invalid number for dimension" << argv[4] << '\n';
+
 	return 0;
 }
